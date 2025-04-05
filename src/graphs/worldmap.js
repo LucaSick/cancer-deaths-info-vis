7f604e7a3483e909d2bd428b6eb1630a08b5fbef
MircoGijsens1 <mirco.gijsens@gmail.com>
2025-04-05 07:03:13 +0200

    Added language, zooming selecting country

--- src/graphs/worldmap.js
+++ src/graphs/worldmap.js
@@ -8,22 +8,35 @@ import FilterValues from '../Utils/filterValues';
 //https://technology.amis.nl/frontend/world-map-data-visualization-with-d3-js-geojson-and-svg-zooming-and-panning-and-dragging/
 //And https://www.d3indepth.com/geographic/
 
-const container = document.getElementById("world_container");
-const width = container.clientWidth;
-const height = container.clientHeight;
+
 let geoData;
 const GEOJSON = "/GEOJSON_medium_stripped.json"
 const storageKey = "worlmap_filter"
 let rawCSVData = [];
 const padding = 20;
+const legendHeight = 100
+const titleHeight = 50
+const globalHeight = 20
+let zoom
 let dataMap = new Map()
-let worldmap, worldmapGroup, projection, geoGenerator;
+let worldmap, titleGroup, worldmapGroup, globalGroup, legendGroup, projection, geoGenerator, width, height
+let mapHeight
+let selectedCountry
+let globaleData = {}
+const legendLabel = {
+    "Rate": "Rate per 100 000",
+    "Percent": "Percent of all possible deaths",
+    "Number": "Number of deaths"
+}
 
 //GET the GEOJSONDATA
 d3.json(GEOJSON).then(data => {
     geoData = data;
-    drawMap();
+    populateFilters()
+
+    CreateMapSVG();
     loadCSV();
+
 }).catch((error) => {
     console.error("Something went wrong loading the data: ", error);
 });
@@ -75,28 +88,66 @@ const populateFilters = () => {
         return option;
     });
     ageSelect.append(...ageOptionElements);
+    const countrySelect = document.getElementById("countrySelect");
+    const countryOptionElements = geoData.features.sort((a, b) =>
+        a.properties.name.localeCompare(b.properties.name)
+    ).map(value => {
+        const option = document.createElement("option");
+        option.value = value.properties.name;
+        option.textContent = value.properties.name;
+        return option;
+    });
+    countrySelect.append(...countryOptionElements);
 
     document.querySelector(`#displayToggle .filter-btn[data-value="${defaultValueDisplay}"]`)?.classList.add('active');
     document.querySelector(`#sexToggle .filter-btn[data-value="${defaultValueSex}"]`)?.classList.add('active');
 }
 
-populateFilters()
 //Function to draw the map
-const drawMap = () => {
+const CreateMapSVG = () => {
     const container = document.getElementById("world_container");
     container.style.height = `${container.clientHeight}px`;
-    const width = container.clientWidth;
-    const height = container.clientHeight;
+    width = container.clientWidth;
+    height = container.clientHeight;
+    mapHeight = height - titleHeight - globalHeight - legendHeight - padding;
     d3.select("#world_container").select("svg").remove();
     worldmap = d3.select("#world_container")
         .append("svg")
         .attr("width", width)
         .attr("height", height)
-        .style("display", "block");;
+        .style("display", "block");
 
-    worldmapGroup = worldmap.append("g").attr("id", "world_map");
-    projection = d3.geoMercator().fitExtent(
-        [[padding, padding], [width - padding, height - padding]], geoData)
+    worldmap.append("defs").append("clipPath")
+        .attr("id", "map-clip")
+        .append("rect")
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("width", width)
+        .attr("height", mapHeight);
+
+    titleGroup = worldmap.append("g").attr("id", "map_title").attr("transform", "translate(0, 30)")
+    globalGroup = worldmap.append("g").attr("id", "globalInfo").attr("transform", `translate(${0}, ${titleHeight + 30})`)
+    const zoomGroup = worldmap.append("g").attr("id", "zoomGroup").attr("clip-path", "url(#map-clip)").attr("transform", `translate(${0}, ${titleHeight + globalHeight + padding})`);
+    worldmapGroup = zoomGroup.append("g").attr("id", "world_map");
+    legendGroup = worldmap.append("g")
+        .attr("id", "map_legend")
+        .attr("transform", `translate(${padding}, ${height - legendHeight})`);
+
+    zoom = d3.zoom()
+        .scaleExtent([1, 15])
+        //.translateExtent([[0, 0], [width, height]])
+        .on("zoom", (event) => {
+            worldmapGroup.attr("transform", event.transform);
+            //worldmapGroup.attr("transform", event.transform);
+        });
+
+    worldmap.call(zoom);
+    redrawMap()
+}
+const redrawMap = () => {
+    worldmap.select("path").remove()
+    projection = d3.geoEquirectangular().fitExtent(
+        [[padding, padding], [width - padding, mapHeight]], geoData)
     geoGenerator = d3.geoPath()
         .projection(projection);
     worldmapGroup.selectAll('path')
@@ -105,9 +156,9 @@ const drawMap = () => {
         .attr('d', geoGenerator)
         .attr("data-country", d => d.properties.name)
         .classed("Country_map", true)
+    //.classed("Monaco", d => d.properties.name === "Monaco")
     UpdateView()
 }
-
 //Load the correct CSV file
 const loadCSV = () => {
     const properties = LocalstorageProperties.getProperties(storageKey)
@@ -128,6 +179,7 @@ const updateMapFromCSV = () => {
 //add the country colors etc.
 const UpdateView = () => {
     const properties = LocalstorageProperties.getProperties(storageKey)
+    createTitle(properties)
     const metric = `${properties.view_type}_val`;
     const allValues = Array.from(dataMap.values()).map(d => +d[metric])
     const populationDomain = d3.extent(allValues)
@@ -158,6 +210,138 @@ const UpdateView = () => {
             }
             return d.properties.name;
         });
+    createLegend(populationDomain, colorScale, metric, properties)
+}
+
+
+const createTitle = (properties) => {
+    titleGroup.selectAll("*").remove();
+    globalGroup.selectAll("*").remove();
+    titleGroup.append("text")
+        .attr("x", width / 2) // center it
+        .attr("y", 0) // some spacing below the axis
+        .attr("text-anchor", "middle")
+        .attr("font-size", "25px")
+        .attr("fill", "#000")
+        .style("font-weight", "bold")
+        .text(`${properties.cause_name} (${properties.year})`);
+    titleGroup.append("text")
+        .attr("x", width / 2)
+        .attr("y", 25)
+        .attr("text-anchor", "middle")
+        .attr("font-size", "14px")
+        .attr("fill", "#555")
+        .text(`Ages: ${properties.age_name}`);
+
+    const lower = globalGroup.append("text")
+        .attr("x", padding)
+        .attr("y", 0)
+        .attr("text-anchor", "start")
+        .attr("font-size", "14px")
+        .attr("fill", "#555")
+        .style("font-weight", "bold")
+        .text(`Lower (Global): `);
+    const estimate = globalGroup.append("text")
+        .attr("x", width / 2)
+        .attr("y", 0)
+        .attr("text-anchor", "middle")
+        .attr("font-size", "14px")
+        .attr("fill", "#555")
+        .style("font-weight", "bold")
+        .text(`Estimate (Global): `);
+    const upper = globalGroup.append("text")
+        .attr("x", width - padding)
+        .attr("y", 0)
+        .attr("text-anchor", "end")
+        .attr("font-size", "14px")
+        .attr("fill", "#555")
+        .style("font-weight", "bold")
+        .text(`Upper (Global): `);
+    lower.append("tspan")
+        .attr("dy", 0)
+        .style("font-weight", "normal")
+        .text((+globaleData[`${properties.view_type}_lower`]).toFixed(2));
+    estimate.append("tspan")
+        .attr("dy", 0)
+        .style("font-weight", "normal")
+        .text((+globaleData[`${properties.view_type}_val`]).toFixed(2));
+    upper.append("tspan")
+        .attr("dy", 0)
+        .style("font-weight", "normal")
+        .text((+globaleData[`${properties.view_type}_upper`]).toFixed(2));
+}
+const createLegend = (populationDomain, colorScale, metric, properties) => {
+    legendGroup.selectAll("*").remove();
+    const mapGradient = legendGroup
+        .append("defs")
+        .append("linearGradient")
+        .attr("id", "mapGradient")
+        .attr("x1", "0%")
+        .attr("y1", "0%")
+        .attr("x2", "100%")
+        .attr("y2", "0%");
+    const steps = 10;
+    const step = (populationDomain[1] - populationDomain[0]) / steps;
+    for (let i = 0; i <= steps; i++) {
+        const val = populationDomain[0] + i * step;
+        mapGradient.append("stop")
+            .attr("offset", `${(i / steps) * 100}%`)
+            .attr("stop-color", colorScale(val));
+    }
+    const legendHeight = 25
+    const rectWidth = width - padding * 2;
+    legendGroup.append("rect")
+        .attr("x", 0)
+        .attr("y", 10)
+        .attr("width", rectWidth)
+        .attr("height", legendHeight)
+        .style("fill", "url(#mapGradient)")
+        .style("stroke", "#000")
+        .style("stroke-width", 1);
+
+
+    const xAxisScale = d3.scaleLinear()
+        .domain(populationDomain)
+        .range([0, rectWidth]);
+    const xAxis = d3.axisBottom(xAxisScale)
+        .ticks(6)
+        .tickFormat(d3.format(".2f"));
+
+    legendGroup.append("g")
+        .attr("transform", `translate(0, ${10 + legendHeight})`)
+        .call(xAxis);
+
+    legendGroup.append("text")
+        .attr("x", rectWidth / 2) // center it
+        .attr("y", legendHeight + 50) // some spacing below the axis
+        .attr("text-anchor", "middle")
+        .attr("font-size", "15px")
+        .attr("fill", "#000")
+        .style("font-weight", "bold")
+        .text(legendLabel[properties.view_type]);
+
+    const countryData = Array.from(dataMap.values())
+    legendGroup.selectAll(".legend-dot")
+        .data(countryData)
+        .join("circle")
+        .attr("cx", d => xAxisScale(d[metric]))
+        .attr("cy", 0)
+        .attr("r", 5)
+        .style("fill", d => colorScale(d[metric]))
+        //.style("opacity", 0.5)
+        .style("stroke", "#000")
+        .style("stroke-width", 0.5)
+        .on("click", (event, d) => {
+            zoomToACountry(d.geo_location_name);
+        })
+        .append("title")
+        .text(d => {
+            const name = d.location_name;
+            const estimate = (+d[metric]).toFixed(2) || "N/A";
+            const upper = (+d[`${properties.view_type}_upper`]).toFixed(2) || "N/A";
+            const lower = (+d[`${properties.view_type}_lower`]).toFixed(2) || "N/A";
+            return `${name}\nLower: ${lower}\nEstimate: ${estimate}\nUpper: ${upper}`;
+        });
 }
 
 //Function to create a map of the CSV data
@@ -170,7 +354,9 @@ const createCSVMapCountryMap = (data) => {
 //filter the CSV data
 const filterCSVData = (data) => {
     const properties = LocalstorageProperties.getProperties(storageKey)
-    return data.filter(d => d["age_name"] === properties.age_name && d["cause_name"] === properties.cause_name && +d["year"] === properties.year)
+    const globalName = "Global";
+    globaleData = data.find(d => d["location_name"] === globalName)
+    return data.filter(d => d["location_name"] !== globalName && d["age_name"] === properties.age_name && d["cause_name"] === properties.cause_name && +d["year"] === properties.year)
 }
 
 //Create the path to the right csv file
@@ -181,16 +367,94 @@ const getCSVName = (properties) => {
     return `/worldmap/${cancer}/${sex}/${cancer}_${sex}_${year}.csv`
 }
 
+// find largest polygon for multypoligon countries like russia
+const getLargestPolygon = (countryName) => {
+    const countryFeature = geoData.features.find(d => d.properties.name === countryName);
+    if (countryFeature.geometry.type !== "MultiPolygon") {
+        return countryFeature;
+    }
+    const largestPolygon = countryFeature.geometry.coordinates.reduce((acc, coords) => {
+        const area = d3.geoArea({ type: "Polygon", coordinates: coords });
+
+        return area > acc.area
+            ? { coords, area }
+            : acc;
+    }, { coords: null, area: -Infinity });
+    return {
+        type: "Feature",
+        properties: countryFeature.properties,
+        geometry: {
+            type: "Polygon",
+            coordinates: largestPolygon.coords
+        }
+    };
+
+}
+const zoomToACountry = (countryName) => {
+    selectedCountry = countryName
+    const countryFeature = getLargestPolygon(countryName);
+    d3.selectAll(".Country_map").classed("Focus", false);
+
+    d3.selectAll(`.Country_map[data-country="${countryName}"]`)
+        .classed("Focus", true).raise();
+
+    const [[x0, y0], [x1, y1]] = geoGenerator.bounds({
+        type: "Feature",
+        properties: {},
+        geometry: countryFeature.geometry
+    });
+    const dx = x1 - x0;
+    const dy = y1 - y0;
+    const x = (x0 + x1) / 2;
+    const y = (y0 + y1) / 2;
+    const newScale = Math.max(1, Math.min(8, 0.9 / Math.max(dx / width, dy / mapHeight)));
+    const transform = d3.zoomIdentity
+        .translate(width / 2, mapHeight / 2)
+        .scale(newScale)
+        .translate(-x, -y);
+
+    worldmap.transition()
+        .duration(1000)
+        .call(
+            zoom.transform,
+            transform
+        );
+}
+const clearSelectedCountry = () => {
+    if (selectedCountry) {
+        selectedCountry = null;
+        d3.selectAll(".Country_map").classed("Focus", false);
+        document.getElementById("countrySelect").value = "";
+    }
+};
+const resetZoom = () => {
+    const transform = d3.zoomIdentity
+        .translate(0, 0)
+        .scale(1)
+
+    worldmap.transition().duration(750)
+        .call(zoom.transform, transform);
+}
+
+
 //Resizing the window
 let resizeTimeout;
 
 window.addEventListener("resize", () => {
+    clearSelectedCountry()
     clearTimeout(resizeTimeout);
     resizeTimeout = setTimeout(() => {
         requestAnimationFrame(() => {
-            if (geoData) drawMap();
+            if (geoData) CreateMapSVG();
         })
     }, 200);
+});
+
+//Country events
+
+document.getElementById("resetZoomBtn").addEventListener("click", (e) => {
+    clearSelectedCountry()
+    resetZoom()
 });
 
 //Filter events
@@ -200,6 +464,7 @@ document.getElementById("cancerSelect").addEventListener("change", (e) => {
     const value = e.target.value;
     console.log("Cancer type selected:", value);
     LocalstorageProperties.setPropreties(storageKey, { cause_name: value })
+    //clearSelectedCountry()
     loadCSV()
     // handleChange("cancer", value);
 });
@@ -214,6 +479,7 @@ sexButtons.forEach(btn => {
         const value = btn.dataset.value;
         console.log("Sex selected:", value);
         LocalstorageProperties.setPropreties(storageKey, { sex: value.toLowerCase() })
+        //clearSelectedCountry()
         loadCSV()
         // handleChange("sex", value);
     });
@@ -224,6 +490,7 @@ document.getElementById("yearSelect").addEventListener("change", (e) => {
     const value = e.target.value;
     console.log("Year selected:", value);
     LocalstorageProperties.setPropreties(storageKey, { year: +value })
+    //clearSelectedCountry()
     loadCSV()
     // handleChange("year", value);
 });
@@ -233,6 +500,7 @@ document.getElementById("ageSelect").addEventListener("change", (e) => {
     const value = e.target.value;
     console.log("Age group selected:", value);
     LocalstorageProperties.setPropreties(storageKey, { age_name: value })
+    //clearSelectedCountry()
     updateMapFromCSV()
     // handleChange("age_group", value);
 });
@@ -247,6 +515,13 @@ displayButtons.forEach(btn => {
         console.log("Sex selected:", value);
         LocalstorageProperties.setPropreties(storageKey, { view_type: value })
         UpdateView()
+        //clearSelectedCountry()
         // handleChange("sex", value);
     });
+});
+//Country
+document.getElementById("countrySelect").addEventListener("change", (e) => {
+    const value = e.target.value;
+    zoomToACountry(value)
+    // handleChange("age_group", value);
 });
