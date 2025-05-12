21773deb0cb12e781ff8bc4ad6fb318474cf4529
MircoGijsens1 <mirco.gijsens@gmail.com>
2025-05-12 22:23:42 +0200

    Added accordion and fixed bug

--- src/graphs/worldmap.js
+++ src/graphs/worldmap.js
@@ -45,6 +45,22 @@ const metric_product = {
     "Number": 1
 }
 
+// accordion code
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+
 //Create the navigation bar making sure to specify that the worldmap is selected.
 createLoadNavbar("Worldmap")
 
@@ -99,7 +115,6 @@ const populateFilters = () => {
     const defaultValueAge = properties.age_name;
     const defaultValueSex = properties.sex;
     const defaultValueDisplay = properties.view_type;
-    currentYear = defaultValueYear;
 
     const yearRange = document.getElementById("yearRange");
     yearRange.value = defaultValueYear;
